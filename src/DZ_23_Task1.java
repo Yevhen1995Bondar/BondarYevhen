0b9fb854d6cdaa50f6fbfa8cb08b3f081b711faf
Yevhen1995Bondar <145388532+Yevhen1995Bondar@users.noreply.github.com>
2023-10-27 21:57:49 +0200

    Variant 12

--- src/DZ_23_Task1.java
+++ src/DZ_23_Task1.java
@@ -1,8 +1,8 @@
 public class DZ_23_Task1 {
     public static void main(String[] args) {
-        String імя = "Вася";
-        int вік = 30;
-        double вага = 60.5;
+        String імя = "Yevhen";
+        int вік = 28;
+        double вага = 82.3;
 
         System.out.println("Імя=" + імя + ", Вік=" + вік + ", Вага=" + вага);
     }
