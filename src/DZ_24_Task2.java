54d2f84ef73b2ab706017bce65e8962f0da9867a
Yevhen1995Bondar <145388532+Yevhen1995Bondar@users.noreply.github.com>
2023-10-28 15:01:04 +0200

    Variant 12

--- src/DZ_24_Task2.java
+++ src/DZ_24_Task2.java
@@ -9,6 +9,14 @@ public class DZ_24_Task2 {
         System.out.println("xB = " + xB + ", yB = " + yB);
         System.out.println("xC = " + xC + ", yC = " + yC);
 
+        double area = calculateTriangleArea(xA, yA, xB, yB, xC, yC);
+
+        System.out.println("Площа трикутника: " + area);
     }
 
+    public static double calculateTriangleArea(double xA, double yA, double xB, double yB, double xC, double yC) {
+        double area = Math.abs((xA * (yB - yC) + xB * (yC - yA) + xC * (yA - yB)) / 2);
+        return area;
+    }
 }
+
