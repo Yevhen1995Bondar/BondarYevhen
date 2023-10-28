c70499f0898b9a1e7a14be30da16b96751555025
Yevhen1995Bondar <145388532+Yevhen1995Bondar@users.noreply.github.com>
2023-10-28 14:49:10 +0200

    Variant 12

---
+++ src/DZ_24_Task2.java
@@ -0,0 +1,14 @@
+public class DZ_24_Task2 {
+    public static void main(String[] args) {
+        double xA = 1, yA = 5;
+        double xB = 3, yB = 5;
+        double xC = 6, yC = 7;
+
+        System.out.println("До обчислення:");
+        System.out.println("xA = " + xA + ", yA = " + yA);
+        System.out.println("xB = " + xB + ", yB = " + yB);
+        System.out.println("xC = " + xC + ", yC = " + yC);
+
+    }
+
+}
