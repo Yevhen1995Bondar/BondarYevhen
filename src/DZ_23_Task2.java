efcf9e58434ec686e55af73ad331deab56281287
Yevhen1995Bondar <145388532+Yevhen1995Bondar@users.noreply.github.com>
2023-10-28 12:45:29 +0200

    Variant 12

---
+++ src/DZ_23_Task2.java
@@ -0,0 +1,19 @@
+public class DZ_23_Task2 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        System.out.println("До обміну:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Обмін значень без прямого присвоєння
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("Після обміну:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
