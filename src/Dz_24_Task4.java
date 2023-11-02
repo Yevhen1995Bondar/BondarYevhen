dcfdc8f2209de088901046ada16be3e425b1b501
Yevhen1995Bondar <145388532+Yevhen1995Bondar@users.noreply.github.com>
2023-11-02 11:22:35 +0100

    Variant 12

---
+++ src/Dz_24_Task4.java
@@ -0,0 +1,26 @@
+public class Dz_24_Task4 {
+
+    public static void main(String[] args) {
+        double num1 = -5;
+        double num2 = 7;
+        double num3 = 11;
+
+        double minAbsNum = findMinAbsoluteNumber(num1, num2, num3);
+
+        System.out.println("Менше за модулем число: " + minAbsNum);
+    }
+
+    public static double findMinAbsoluteNumber(double num1, double num2, double num3) {
+        double absNum1 = Math.abs(num1);
+        double absNum2 = Math.abs(num2);
+        double absNum3 = Math.abs(num3);
+
+        if (absNum1 <= absNum2 && absNum1 <= absNum3) {
+            return num1;
+        } else if (absNum2 <= absNum1 && absNum2 <= absNum3) {
+            return num2;
+        } else {
+            return num3;
+        }
+    }
+}
